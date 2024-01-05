913536a5f75970e4da0bb84b5423a7b9cc1e24ea
Simple Guruji <dashingak957014@gmail.com>
2024-01-05 13:54:47 +0530

    Created changeCurrentPassword,   getCurrentUser,   updateAccountDetails,   updateUserAvatar and   updateUserCoverImage routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -29,5 +35,19 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJwt, logoutUser);
 
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+
+router.route("/update-account").patch(verifyJwt, updateAccountDetails);
+
+router
+  .route("/avatar")
+  .patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/cover-image")
+  .patch(verifyJwt, upload.single("cover-image"), updateUserCoverImage);
 
 export default router;
